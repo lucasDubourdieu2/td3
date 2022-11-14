7164dc59e6b4a7132772f123059843a4c6fa74f5
LD3il <117454985+lucasDubourdieu2@users.noreply.github.com>
2022-11-14 09:45:05 +0000

    correction probleme replit

--- Exercice2/Point1.java
+++ Exercice2/Point1.java
@@ -1,6 +1,27 @@
 public class Point1 {
 
+  public Point1() {
+  }
+
+  public Point1(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public void setPoint(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public void deplace(int dx, int dy) {
+    x += dx;
+    y += dy;
+  }
+
+  public void afficheCoords() {
+    System.out.println("Coordonnées : " + x + " " + y);
+  }
+
+  private int x, y;
+
 }
-
-  
-
